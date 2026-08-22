4fff2afee0ae24f97c08cb215cfdcf92fc525357
Esteban <esteban.horacio.acevedo@gmail.com>
2026-08-22 11:07:06 -0300

    Uso de extends y block

--- src/core/urls.py
+++ src/core/urls.py
@@ -6,10 +6,15 @@ app_name = "core"
 
 urlpatterns = [
     path("", views.index, name="home"),
-    path("saludar/", views.saludar),
-    path("saludar-tag/", views.saludar_tag),
-    path("parametros/<str:nombre>/<str:apellido>/", views.parametros_ruta),
-    path("ejercicio1/", views.ejercicio1),
-    path("notas/", views.ver_notas, name="notas"),
-    path("ejercicio2/", views.ejercicio2, name="ejercicio2"),
 ]
+
+# Viejo
+# urlpatterns = [
+#     path("", views.index, name="home"),
+#     path("saludar/", views.saludar),
+#     path("saludar-tag/", views.saludar_tag),
+#     path("parametros/<str:nombre>/<str:apellido>/", views.parametros_ruta),
+#     path("ejercicio1/", views.ejercicio1),
+#     path("notas/", views.ver_notas, name="notas"),
+#     path("ejercicio2/", views.ejercicio2, name="ejercicio2"),
+# ]
